7d6ec3c7419f581b7d2556e2e7831ca04b84d05c
Shirisha <shirisha.dharavath@translense.com>
2025-03-23 07:58:54 +0530

    Delete src directory

--- src/Component/Welcome.js
+++
@@ -1,7 +0,0 @@
-import React, {Component} from "react";
-class Welcome extends Component {
-    render() {
-        return<h1>welcome {this.props.name} a.ka. {this.props.heroname}</h1>
-    }
-}
-export default Welcome
